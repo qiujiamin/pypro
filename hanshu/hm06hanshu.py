f1e0608d7970b980f37cb7498fbf304e6eb36758
qiujiamin <34052782+qiujiamin@users.noreply.github.com>
2021-03-16 10:00:14 +0800

    Delete hanshu directory

--- hanshu/hm06hanshu.py
+++
@@ -1,9 +0,0 @@
-def multiple_tables():
-    row =1
-    while row<=9:
-        col =1
-        while col <=row:
-            print("%d * %d = %d" %(col,row,col* row),end=" ")
-            col+=1
-        print("")
-        row+=1
